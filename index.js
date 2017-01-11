95ff276d497d56b59a1719923a2fb3dba867ce04
Bryan Jackson <bjack13@gmail.com>
2017-01-11 16:29:24 -0700

    inserting products into database

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const massive = require('massive');
 
-const app = express();
+const connectionString = "postgress://localhost/massive-node-project";
+const massiveInstance = massive.connectSync({connectionString:connectionString});
+
+const app = module.exports = express();
 const config = require('./config');
 
+app.set('db',massiveInstance);
+
+const db = app.get('db');
 
 app.use(bodyParser.json());
 app.use(cors());
